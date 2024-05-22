236300d89f3f37353a08e3b09cdc65047760e631
Ej Myles <ejelymyles@gmail.com>
2024-05-21 19:59:33 -0700

    initial setup

--- server/app.py
+++ server/app.py
@@ -3,15 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import ##put models here
 
-
-# Views go here!
 
 @app.route('/')
 def index():
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,12 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db ##import new models 
 
 if __name__ == '__main__':
+
     fake = Faker()
+
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
