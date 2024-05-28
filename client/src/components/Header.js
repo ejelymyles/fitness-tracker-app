93c9b5d9f90dc697d3c551b58c981ce17201ce55
Ej Myles <ejelymyles@gmail.com>
2024-05-28 11:42:11 -0700

    build Header.js starter code

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header(){
+    return (
+        <div className="header">
+            <h1 className="header-title">Welcome to Fitness App!</h1>
+            <h4 className="header-subtitle">Find new movements, track your workouts, stay on top of your goals!</h4>
+            <NavBar />
+        </div>
+    );
+}
+
+export default Header;
